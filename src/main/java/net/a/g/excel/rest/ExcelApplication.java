6a01204a71d707ff8bad224976424c35f67503af
admin <gautric@redhat.com>
2021-10-08 16:37:21 +0200

    API Documentation

--- src/main/java/net/a/g/excel/rest/ExcelApplication.java
+++ src/main/java/net/a/g/excel/rest/ExcelApplication.java
@@ -2,6 +2,10 @@ package net.a.g.excel.rest;
 
 import javax.ws.rs.ApplicationPath;
 
+import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
+import org.eclipse.microprofile.openapi.annotations.info.Info;
+
+@OpenAPIDefinition(info = @Info(title = "Excel REST API", version = "1.0"))
 @ApplicationPath("api")
 public class ExcelApplication extends javax.ws.rs.core.Application {
 
