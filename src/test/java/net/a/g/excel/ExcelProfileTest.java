620c79bceb3b48a5aa10be4fb2d5e116e601f01b
admin <gautric@redhat.com>
2021-10-11 14:45:34 +0200

    cleanup code

--- src/test/java/net/a/g/excel/ExcelProfileTest.java
+++ src/test/java/net/a/g/excel/ExcelProfileTest.java
@@ -10,7 +10,7 @@ public class ExcelProfileTest implements QuarkusTestProfile {
 	public ExcelProfileTest() {}
 
 	public Map<String, String> getConfigOverrides() {
-		Map<String, String> map = new HashMap();
+		Map<String, String> map = new HashMap<String, String>();
 		map.put("excel.static.resouces.uri", "KYC.xlsx");
 		return map;
 	}
