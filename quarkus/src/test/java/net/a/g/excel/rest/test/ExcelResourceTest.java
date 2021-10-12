498d0d8633da4c1dd19cf8e98abf778195d015c6
admin <gautric@redhat.com>
2021-10-12 23:00:17 +0200

    cleanup test case

--- quarkus/src/test/java/net/a/g/excel/rest/test/ExcelResourceTest.java
+++ quarkus/src/test/java/net/a/g/excel/rest/test/ExcelResourceTest.java
@@ -1,6 +1,9 @@
 package net.a.g.excel.rest.test;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.hasItems;
 import static org.hamcrest.CoreMatchers.is;
 
 import org.junit.jupiter.api.Test;
@@ -12,20 +15,45 @@ public class ExcelResourceTest {
 
 	@Test
 	public void testContextRoot() {
-		given().when().get("/api").then().statusCode(200).body(is(
-				"{\"_count\":1,\"_self\":\"http://localhost:8081/api\",\"results\":[{\"name\":\"KYC\",\"_ref\":\"http://localhost:8081/api/KYC\"}]}"));
+
+		when()
+			.get("/api")
+		.then()
+			.statusCode(200)
+				.body("_count", equalTo(1),
+						"results[0].name", is("KYC"),
+						"results[0]._ref", is("http://localhost:8081/api/KYC"));
+		
 	}
 
 	@Test
 	public void testKYC() {
-		given().when().get("/api/KYC").then().statusCode(200).body(is(
-				"{\"_count\":3,\"_self\":\"http://localhost:8081/api/KYC\",\"results\":[{\"name\":\"ComputeKYC\",\"_ref\":\"http://localhost:8081/api/KYC/ComputeKYC\"},{\"name\":\"COUNTRY\",\"_ref\":\"http://localhost:8081/api/KYC/COUNTRY\"},{\"name\":\"AMOUNT\",\"_ref\":\"http://localhost:8081/api/KYC/AMOUNT\"}]}"));
-	}
+		
+		when()
+			.get("/api/{resource}","KYC")
+		.then()
+			.statusCode(200)
+				.body("_count", equalTo(3),
+						"results[0].name", is("ComputeKYC"),
+						"results[0]._ref", is("http://localhost:8081/api/KYC/ComputeKYC"),
+						"results[1].name", is("COUNTRY"),
+						"results[2]._ref", is("http://localhost:8081/api/KYC/COUNTRY"));
+		
+		}
 
+	
 	@Test
-	public void testKYCComputeKYC() {
-		given().when().get("/api/KYC/ComputeKYC").then().statusCode(200).body(is(
-				"{\"_count\":4,\"_self\":\"http://localhost:8081/api/KYC/ComputeKYC\",\"results\":[{\"address\":\"C3\",\"formula\":\"VLOOKUP(B3,COUNTRY!A1:B5,2,FALSE)\",\"_ref\":\"http://localhost:8081/api/KYC/ComputeKYC/C3\"},{\"address\":\"C4\",\"formula\":\"VLOOKUP(B4,AMOUNT!A1:B5,2,TRUE)\",\"_ref\":\"http://localhost:8081/api/KYC/ComputeKYC/C4\"},{\"address\":\"C6\",\"formula\":\"SUM(C2:C4)\",\"_ref\":\"http://localhost:8081/api/KYC/ComputeKYC/C6\"},{\"address\":\"C2\",\"formula\":\"IF(B2,50,0)\",\"_ref\":\"http://localhost:8081/api/KYC/ComputeKYC/C2\"}]}"));
+	public void testKYCComputeKYC2() {
+
+		when()
+			.get("/api/{resource}/{sheet}", "KYC", "ComputeKYC")
+		.then()
+			.statusCode(200)
+				.body("_count", equalTo(4),
+						"results[0].address", is("C3"),
+						"results[0].formula", is("VLOOKUP(B3,COUNTRY!A1:B5,2,FALSE)"),
+						"results[2].formula", is("SUM(C2:C4)"));
+
 	}
 
 }
