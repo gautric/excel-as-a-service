d16d8d16a97ea704e8b3d7aabc507479e047c938
admin <gautric@redhat.com>
2021-10-12 23:58:11 +0200

    add Logger when error

--- quarkus/src/main/java/net/a/g/excel/rest/ExceptionHandler.java
+++ quarkus/src/main/java/net/a/g/excel/rest/ExceptionHandler.java
@@ -7,12 +7,22 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import net.a.g.excel.model.ExcelError;
 import net.a.g.excel.model.ExcelResult;
 
 @Provider
 public class ExceptionHandler implements ExceptionMapper<Exception> {
+	
+	public final static Logger LOG = LoggerFactory.getLogger(ExceptionHandler.class);
+
 	public Response toResponse(Exception exception) {
+		
+		LOG.error("REST API Catch Exception", exception);
+
+		
 		ExcelResult ret = new ExcelResult();
 		ExcelError err = new ExcelError();
 
@@ -23,7 +33,7 @@ public class ExceptionHandler implements ExceptionMapper<Exception> {
 		err.setCode(exception.getClass().getCanonicalName());
 		err.setError(sw.toString());
 		ret.setError(err);
-
+		
 		return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(ret).build();
 	}
 
