29fe08d91768e43aa62e4f9f5e14d0277846c6f5
admin <gautric@redhat.com>
2021-10-16 13:48:27 +0200

    add Main Quarkus App

---
+++ quarkus/src/main/java/net/a/g/excel/main/Main.java
@@ -0,0 +1,12 @@
+package net.a.g.excel.main;
+
+import io.quarkus.runtime.Quarkus;
+import io.quarkus.runtime.annotations.QuarkusMain;
+
+@QuarkusMain  
+public class Main {
+
+    public static void main(String ... args) {
+        Quarkus.run(args); 
+    }
+}
