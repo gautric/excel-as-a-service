7a48ea762a481407b479b3de23e869e5d0d58468
admin <gautric@redhat.com>
2021-10-20 13:03:22 +0200

    add documentation

--- quarkus/src/main/java/net/a/g/excel/rest/ExcelApplication.java
+++ quarkus/src/main/java/net/a/g/excel/rest/ExcelApplication.java
@@ -3,9 +3,10 @@ package net.a.g.excel.rest;
 import javax.ws.rs.ApplicationPath;
 
 import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
+import org.eclipse.microprofile.openapi.annotations.info.Contact;
 import org.eclipse.microprofile.openapi.annotations.info.Info;
 
-@OpenAPIDefinition(info = @Info(title = "Excel REST API", version = "1.0"))
+@OpenAPIDefinition(info = @Info(title = "Excel as a Service", version = "1.0", description = "Engine to compute Excel Resource as a Service", contact = @Contact(email = "gautric@redhat.com", name = "Greg I/O", url = "https://github.com/gautric/excel-as-a-service")))
 @ApplicationPath("api")
 public class ExcelApplication extends javax.ws.rs.core.Application {
 
