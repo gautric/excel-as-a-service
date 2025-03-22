d6fe1bdd13aab55b31248705c91e701365ee9707
gautric <github@autric.net>
2025-03-22 17:22:11 +0100

    add comment

--- excel-engine/src/main/java/net/a/g/excel/load/ExcelLoader.java
+++ excel-engine/src/main/java/net/a/g/excel/load/ExcelLoader.java
@@ -2,8 +2,21 @@ package net.a.g.excel.load;
 
 import java.io.InputStream;
 
+/**
+ * Interface for loading Excel resources into the system.
+ * Provides functionality to inject Excel workbooks from input streams
+ * into the Excel processing system.
+ */
 public interface ExcelLoader {
 
-	boolean injectResource(String resourceName, String resourceFileName, InputStream resourceStream);
+    /**
+     * Injects an Excel resource into the system from an input stream.
+     * 
+     * @param resourceName the unique name to identify the resource in the system
+     * @param resourceFileName the original file name of the resource (can be null)
+     * @param resourceStream the input stream containing the Excel workbook data
+     * @return true if the resource was successfully injected, false otherwise
+     */
+    boolean injectResource(String resourceName, String resourceFileName, InputStream resourceStream);
 
 }
